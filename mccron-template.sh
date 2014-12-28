10a571224b4c55cf002a0df9880eed4d654d7905
ekultails <ekultails@gmail.com>
2014-12-27 20:40:04 -0500

    Create mccron-template.sh

---
+++ mccron-template.sh
@@ -0,0 +1,19 @@
+#!`which bash`
+#MCRAM - Cron - mccron.sh
+if [[ ! $(ps faux | grep "$mcstart" | grep java | grep -v grep) ]]
+	then #start server
+		screen -dmS mcram sh; #creates a screen "mc" running bash for the server
+		screen -dmS mcrsync sh; #creates a screen for rsyncing the data
+		screen -S mcram -p 0 -X stuff $'rsync -avur $mclocation/ $ramlocation/\n' #rsync into RAM
+		screen -S mcram -p 0 -X stuff $'cd $ramlocation/\n' #this allows the server to properly load the eula.txt license agreement
+		screen -S mcram -p 0 -X stuff $'java -Xmx${mcstartram}M -Xms${mcstartram}M -jar ./$mcstart nogui\n' #start the server
+fi
+
+while [[ 1 -gt 0 ]]; do	
+	sleep 1h;
+	screen -p 0 -S mcram -X eval 'stuff "save-all"\015';
+	sleep 60;
+	screen -S mcrsync -p 0 -X stuff $'rsync -varP $ramlocation/ $mclocation/\n' #rsync the data back to the drive
+done
+
+
