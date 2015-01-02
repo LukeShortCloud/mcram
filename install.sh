7e85cd36121e235e695f2ec8a3d2abba974b7043
ekultails <ekultails@gmail.com>
2015-01-01 22:49:07 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -73,13 +73,14 @@ sed -i s'/$mcstart/'"$mcstart"'/g' ~/mcram/mccron.sh
 sed -i s'/ ${synctime}/'" $synctime"'/g' ~/mcram/mccron.sh
 
 
-if [[ $(type crontab 2>&1 | grep -c "is hashed") -eq 1 ]];
-	then echo -e "$(crontab -l)\n@reboot /bin/sh ~/mcram/mccron.sh" | crontab -
-elif [[ $(type systemctl 2>&1 | grep -c "is hashed") -eq 1 ]];
+if [[ $(type systemctl 2>&1 | grep -c "is hashed") -eq 1 ]];
 	then sed -i s'/$mccronlocation/'"$mccronlocation"'/g' ./mcram.service
 	chmod 750 ./mcram.service
 	sudo cp -af ./mcram.service /usr/lib/systemd/system/mcram.service
 	sudo systemctl enable mcram; sudo systemctl start mcram
+elif [[ $(type crontab 2>&1 | grep -c "is hashed") -eq 1 ]];
+	then echo -e "$(crontab -l)\n@reboot /bin/sh ~/mcram/mccron.sh" | crontab -
+else echo "systemd and crontab are not installed, please install one of these services"
 fi
 
 sudo chmod o+rw /dev/pts/2 #Fixes screen issues when running as the user
