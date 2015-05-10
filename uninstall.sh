4540358fa19419588624c0157da77ea7df788efd
ekultails <ekultails@gmail.com>
2015-05-10 12:54:22 -0400

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -9,11 +9,23 @@ sleep 5s;
 screen -p 0 -S mcram -X eval 'stuff "stop"\015';
 screen -S mcram -X quit
 
+# BROKEN - automatic sed for removing the added MCRAM mounts; likely to be removed in a later version
+#echo "Removing RAM mounts..."
+#sudo chmod 666 /etc/fstab
+#sed -i '/'"$(head -n 1 ~/mcram/mount.uninstall)"'/d' /etc/fstab
+#sudo chmod 644 /etc/fstab
+#sudo mount -a
+
 echo "Removing RAM mounts..."
-sudo chmod 666 /etc/fstab
-sed -i '/'"$(head -n 1 ~/mcram/mount.uninstall)"'/d' /etc/fstab
-sudo chmod 644 /etc/fstab
-sudo mount -a
+echo "Here are the differences between your original and current /etc/fstab"
+diff -y /etc/fstab ~/mcram/etc-fstab
+read -p "Would you like to restore the original /etc/fstab (Y/N)? " restorefstab;
+if [[ $(echo ${restorefstab}) -eq "Y" || (echo ${restorefstab}) -eq "y" ]];
+  then currentdate=$(date +%Y-%m-%d_%Hh.%Mm.%Ss)
+  sudo mv -v /etc/fstab /etc/fstab${currentdate}
+  sudo cp -v ~/mcram/etc-fstab /etc/fstab
+  mount -a
+fi
 
 #Delete MCRAM files
 echo "Removing all MCRAM files..."
